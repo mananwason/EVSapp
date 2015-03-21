8fcc7fcb74c64366094061af9dc40d97da9fe415
mananwason <manan13056@iiitd.ac.in>
2015-03-21 16:49:06 +0530

    setting activity inserted

--- app/src/main/java/com/group1/evs/iiitd/evsapp/MainActivity.java
+++ app/src/main/java/com/group1/evs/iiitd/evsapp/MainActivity.java
@@ -22,11 +22,6 @@ public class MainActivity extends NavigationLiveo implements NavigationLiveoList
 
     @Override
     public void onUserInformation() {
-        //User information here
-        this.mUserName.setText("Rudson Lima");
-        this.mUserEmail.setText("rudsonlive@gmail.com");
-        this.mUserPhoto.setImageResource(R.drawable.ic_rudsonlive);
-        this.mUserBackground.setImageResource(R.drawable.ic_user_background);
     }
 
     @Override
--- app/src/main/java/com/group1/evs/iiitd/evsapp/SettingsActivity.java
+++ app/src/main/java/com/group1/evs/iiitd/evsapp/SettingsActivity.java
@@ -1,7 +1,30 @@
 package com.group1.evs.iiitd.evsapp;
 
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
 /**
  * Created by manan on 21-03-2015.
  */
-public class SettingsActivity {
+public class SettingsActivity extends ActionBarActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstance){
+        super.onCreate(savedInstance);
+        setContentView(R.layout.fragment_main);
+
+        TextView mtxtTitle = (TextView) findViewById(R.id.txtTitle);
+        mtxtTitle.setText(getString(R.string.settings));
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        finish();
+        return super.onOptionsItemSelected(item);
+    }
 }
